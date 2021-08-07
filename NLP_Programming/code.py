fe00e9980cbf5098b981de517717db8a404857a5
Vignesh_Ss <56245530+vigneshSs-07@users.noreply.github.com>
2021-08-08 01:29:27 +0530

    Extract domain name from Email address and remove domain name

--- NLP_Programming/code.py
+++ NLP_Programming/code.py
@@ -1,1 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Remove domain name
+
+# In[5]:
+
+
+def remove_domain(txt):
+    mail = txt.split('@')[0]    
+    return mail
+
+
+# In[6]:
+
+
+mail = "neethu@gmail.com"
+
+
+# In[7]:
+
+
+remove_domain(mail)
+
+
+# ### Extract domain name from Email address
+
+# In[14]:
+
+
+def remove_domain(txt):
+    mail = txt.split('@')[-1]    
+    return mail
+
+
+# In[15]:
+
+
+mail = "neethu@gmail.com"
+
+
+# In[16]:
+
+
+remove_domain(mail)
+
+
+# ### (or)
+
+# In[17]:
+
+
+import re
+s = 'My name is Conrad, and blahblah@gmail.com is my email.'
+domain = re.search("@[\w.]+", s)
+
+
+# In[19]:
+
+
+domain.group()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
